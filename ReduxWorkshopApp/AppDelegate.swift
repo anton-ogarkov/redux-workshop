2c3b56a658dcc3bd1ab62dd537dee96039e685c5
Anton Ogarkov <anton.ogarkov@sigma.software>
2017-10-05 15:06:42 +0300

    Add Countries view controller

--- ReduxWorkshopApp/AppDelegate.swift
+++ ReduxWorkshopApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,8 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle(identifier: "private.UI"))
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = UINavigationController(rootViewController: UIViewController())
+        self.window?.rootViewController = UINavigationController(rootViewController: mainStoryboard.instantiateViewController(withIdentifier: "CountriesViewController"))
         self.window?.makeKeyAndVisible()
         
         return true
